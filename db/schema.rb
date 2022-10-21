4d29aca730a9175515ea659ae5274d4af7c48d19
Kyrian <dr.kyrian.onwe@gmail.com>
2022-10-21 10:15:57 -0500

    Add columns to users and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_150905) do
+ActiveRecord::Schema.define(version: 2022_10_21_151316) do
 
   create_table "todos", force: :cascade do |t|
     t.string "task"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_10_21_150905) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
+    t.string "role"
+    t.string "location"
   end
 
 end
