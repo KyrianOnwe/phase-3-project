e8b99da24f500c2fa8fae785db335ad4fc1e2c78
Kyrian <dr.kyrian.onwe@gmail.com>
2022-10-21 10:05:51 -0500

    Create todos table

--- db/migrate/20221021150253_create_todos.rb
+++ db/migrate/20221021150253_create_todos.rb
@@ -1,4 +1,10 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
+    create_table :todos do |t|
+      t.string :task
+      t.string :due_date
+      t.integer :owner_id
+      t.timestamps 
+    end
   end
 end
