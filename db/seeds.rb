b20db5e5263b707875bd6b959310db22a18d5e21
Kyrian <dr.kyrian.onwe@gmail.com>
2022-10-21 10:52:06 -0500

    Seed tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,39 @@
+require 'faker'
+
+Todo.destroy_all
+User.destroy_all
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+User.create(
+    name: "Kyrian",
+    role: "Owner",
+    email: "kyrian@owner.com",
+    password: "Hello"
+)
+User.create(
+    name: "Marty McFly",
+    role: "Administrator",
+    email: "marty@mcfly.com",
+    password: "Hello_World!"
+)
+5.times do
+    User.create(
+        name: Faker::Name.unique.name,
+        role: Faker::Job.position,
+        email: Faker::Internet.email,
+        password: "Yoyoyo"
+    )
+end
+50.times do
+    Todo.create(
+        task: Faker::Hobby.activity,
+        due_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+        status: "Assigned",
+        completed: false,
+        assigned_by: "Marty McFly"
+    )
+end
 
 puts "✅ Done seeding!"
